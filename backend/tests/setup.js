90b6de32346118c91b5099877fff4df0be3323a0
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2026-02-01 10:45:27 +0100

    test: add backend test setup and placeholders

---
+++ backend/tests/setup.js
@@ -0,0 +1,10 @@
+/**
+ * test/setup.js
+ * Global setup and teardown for backend tests.
+ */
+require('dotenv').config();
+
+module.exports = async () => {
+  // TODO: Global setup logic
+  console.log('Test setup initialized.');
+};
